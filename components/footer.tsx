2d6a6564d859ca2c6fcbdee2722f21105d2363d8
noimnotanshul <anshuldabgar@gmail.com>
2026-09-02 15:33:42 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Link from "next/link"
-import { Mail, Phone, MapPin } from "lucide-react"
+import { Mail, Phone } from "lucide-react"
 
 export function Footer() {
   return (
